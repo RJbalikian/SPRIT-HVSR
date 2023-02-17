947b086d0022aa220f5745484e65b3d95ea6cca9
RJbalikian <46536937+RJbalikian@users.noreply.github.com>
2023-02-17 15:23:39 -0600

    update inits for better organization

---
+++ __init__.py
@@ -0,0 +1,10 @@
+#__init__.py
+"""
+This module analysis ambient seismic data using the Horizontal to Vertical Spectral Ratio (HVSR) technique
+"""
+
+from hvsr import hvsrtools
+
+__all__=('hvsrtools')
+
+__author__ = 'Riley Balikian'
--- hvsr/__init__.py
+++ hvsr/__init__.py
@@ -1,16 +1,51 @@
-#from hvsr.hvsrtools import (
-#    fileLib,
-#    hvsrCalcs,
-#    ioput,
-#    msgLib,
-#    powspecdens,
-#    readhvsr,
-#    setParams,
-#    utilities
-#)
+#__init__.py
+"""
+This module analysis ambient seismic data using the Horizontal to Vertical Spectral Ratio (HVSR) technique
+"""
 
-from hvsr import hvsr
+#from hvsr import hvsrtools
 
-__all__ = """
-    hvsr
-    """
+from hvsr.hvsrtools import(
+    check_mark,
+    get_char,
+    time_it,
+    message,
+    error,
+    warning,
+    info,
+    checkifpath,
+    input_param,
+    update_shake_metadata,
+    get_metadata,
+    fetch_data,
+    trim_data,
+    generate_ppsds,
+    process_hvsr,
+    hvplot,
+    check_peaks,
+    print_report
+)
+
+__all__ =('hvsr',
+    'hvsrtools',
+    'check_mark',
+    'get_char',
+    'time_it',
+    'message',
+    'error',
+    'warning',
+    'info',
+    'checkifpath',
+    'input_param',
+    'update_shake_metadata',
+    'get_metadata',
+    'fetch_data',
+    'trim_data',
+    'generate_ppsds',
+    'process_hvsr',
+    'hvplot',
+    'check_peaks',
+    'print_report'
+)
+
+__author__ = 'Riley Balikian'
