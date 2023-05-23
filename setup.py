d8f9d73e92595bdc9fb01c64f0685746629bba73
RJbalikian <46536937+RJbalikian@users.noreply.github.com>
2023-05-23 11:22:53 -0500

    config updates

--- setup.py
+++ setup.py
@@ -5,15 +5,4 @@ setup(
     version="0.0.3",
     install_requires=["obspy", "scipy", "matplotlib", "pandas", "numpy","ipykernel", "pyqt5", "tkcalendar"],
     description="A package for processing and analyzing HVSR (Horizontal to Vertical Spectral Ratio) data",
-    author="Riley Balikian",
-    author_email="balikian@illinois.edu",
-    url=["https://github.com/RJbalikian/SPRIT", "https://rjbalikian.github.io/SPRIT/main.html"],
-    license="MIT",
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
-        "Topic :: Scientific/Engineering :: Geophysics",
-        ]
     )
