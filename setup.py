365e81972f59c273dce08210a7533b9c538c1684
RJbalikian <46536937+RJbalikian@users.noreply.github.com>
2023-05-23 11:26:06 -0500

    config updates

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ from setuptools import setup
 
 setup(
     name="sprit",
+    author= "Riley Balikian",
+    author_email = "balikian@illinois.edu",
     version="0.0.3",
     install_requires=["obspy", "scipy", "matplotlib", "pandas", "numpy","ipykernel", "pyqt5", "tkcalendar"],
     description="A package for processing and analyzing HVSR (Horizontal to Vertical Spectral Ratio) data",
