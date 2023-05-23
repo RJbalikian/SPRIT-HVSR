86b795124bd776821f9d348d403dfea25ec21b7e
RJbalikian <46536937+RJbalikian@users.noreply.github.com>
2023-05-23 13:48:14 -0500

    pypi version config updates

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="sprit",
     author= "Riley Balikian",
     author_email = "balikian@illinois.edu",
-    version="0.0.3",
+    version="0.0.9",
     install_requires=["obspy", "scipy", "matplotlib", "pandas", "numpy", "pyqt5", "tkcalendar"],
     description="A package for processing and analyzing HVSR (Horizontal to Vertical Spectral Ratio) data",
     )
