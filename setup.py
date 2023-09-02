11370b27081c9506e6c7d4615cba10f42a77c883
RJbalikian <46536937+RJbalikian@users.noreply.github.com>
2023-09-02 13:49:25 -0500

    update vernum

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name="sprit",
     author= "Riley Balikian",
     author_email = "balikian@illinois.edu",
-    version="0.1.12",
+    version="0.1.13",
     install_requires=["obspy", "scipy", "matplotlib", "pandas", "numpy", "pyqt5", "pyproj"],
     description="A package for processing and analyzing HVSR (Horizontal to Vertical Spectral Ratio) data",
     package_data={'sprit': ['resources/*', 'resources/themes/*', 'resources/themes/forest-dark/*', 'resources/themes/forest-light/*']},
