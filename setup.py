7b423b027b88a8077ec7e03923aeb8a7e280e3c0
RJbalikian <46536937+RJbalikian@users.noreply.github.com>
2023-09-25 13:32:40 -0500

    udpate vernum and specs

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from setuptools import setup, find_packages
-
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="sprit",
     author= "Riley Balikian",
     author_email = "balikian@illinois.edu",
-    version="0.1.32",
+    version="0.1.33",
     package_data={'sprit': ['resources/*', 'resources/themes/*', 'resources/themes/forest-dark/*', 'resources/themes/forest-light/*']},
-    long_description_content_type="text/html",
-    long_description=open("docs/index.html", "r").read(),
+    long_description_content_type="text/markdown",
+    long_description=long_description,
     packages=find_packages(),
     entry_points={
         'console_scripts': [
