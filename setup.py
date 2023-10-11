39e33207ea0bd0a68bc2f7b3a8253386530d33ab
Riley <rileybalikian@outlook.com>
2023-10-11 16:13:48 -0500

    update docs and vernum

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name="sprit",
     author= "Riley Balikian",
     author_email = "balikian@illinois.edu",
-    version="0.1.42",
+    version="0.1.43",
     package_data={'sprit': ['resources/*', 'resources/icon/*', 'resources/themes/*', 'resources/themes/forest-dark/*', 'resources/themes/forest-light/*', 'resources/sample_data/*',]},
     long_description_content_type="text/markdown",
     long_description=long_description,
