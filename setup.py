6cc11c92d954bc267c29d289195eb655ebed7adb
RJbalikian <46536937+RJbalikian@users.noreply.github.com>
2024-01-02 15:58:44 -0600

    update vernum and docs

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name="sprit",
     author= "Riley Balikian",
     author_email = "balikian@illinois.edu",
-    version="0.1.54",
+    version="0.1.55",
     package_data={'sprit': ['resources/*', 'resources/icon/*', 'resources/themes/*', 'resources/themes/forest-dark/*', 
                             'resources/themes/forest-light/*', 'resources/sample_data/*','resources/settings/*']},
     long_description_content_type="text/markdown",
