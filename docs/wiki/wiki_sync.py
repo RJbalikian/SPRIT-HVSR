5c30a9e4cb88f007fd14f817e111336e0ab14a7b
RJbalikian <46536937+RJbalikian@users.noreply.github.com>
2024-02-20 22:59:46 -0600

    clone wiki from github

--- docs/wiki/wiki_sync.py
+++ docs/wiki/wiki_sync.py
@@ -1,9 +1,29 @@
+import os
+import pathlib
+import shutil
+import stat
 import subprocess
 
-upload_wiki=False
-download_wiki=False
+currentDir = pathlib.Path((__file__)).parent
+download_wiki=True
 
-download_cmd=r"git clone git@github.com:RJbalikian/SPRIT-HVSR.wiki.git"
+def onerror(func, path, exc_info):
+    if not os.access(path, os.W_OK):
+        os.chmod(path, stat.S_IWUSR)  # Make the file writable
+        func(path)
+    else:
+        assert False
 
-SHELL_TYPE=True
-subprocess.run(download_cmd, shell=SHELL_TYPE, check=False)
+if download_wiki:
+    dir_path = currentDir.joinpath('SPRIT-HVSR.wiki')
+
+    if dir_path.exists():
+        try:
+            shutil.rmtree(dir_path.as_posix(), onerror=onerror)
+            print(f"Directory '{dir_path}' and its contents have been removed.")
+        except OSError as e:
+            print(f"Error: {e.strerror}")    
+    download_path = currentDir.as_posix()
+    download_cmd=f"git -C {download_path} clone git@github.com:RJbalikian/SPRIT-HVSR.wiki.git"
+    SHELL_TYPE=True
+    subprocess.run(download_cmd, shell=SHELL_TYPE, check=False)
