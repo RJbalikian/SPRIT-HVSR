a9d350745ec35be5ed74bcd24c7f2b8ef8687f73
RJbalikian <46536937+RJbalikian@users.noreply.github.com>
2024-02-20 22:18:15 -0600

    update wikisync

--- docs/wiki/wiki_sync.py
+++ docs/wiki/wiki_sync.py
@@ -1,4 +1,9 @@
+import subprocess
+
 upload_wiki=False
 download_wiki=False
 
 download_cmd=r"git clone git@github.com:RJbalikian/SPRIT-HVSR.wiki.git"
+
+SHELL_TYPE=True
+subprocess.run(download_cmd, shell=SHELL_TYPE, check=False)
