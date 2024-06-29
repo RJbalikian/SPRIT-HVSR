ea35d0cb7a3d0b3f9bad8285af6e163d8945ec5d
RJbalikian <46536937+RJbalikian@users.noreply.github.com>
2024-06-29 14:33:48 -0500

    fix text alignment

--- sprit/sprit_remi.py
+++ sprit/sprit_remi.py
@@ -7,24 +7,29 @@ class SpRIT_REMI(App):
 
     def main(self):
         # Create a TabBox with two tabs
+        spacer50Pct = gui.Label("", width="50%")
+
+
         tab_box = gui.TabBox(width='90%', height="90%")
         
         # First tab
         inputVBox = gui.VBox(width='100%', height='100%')
         
         dateTimeHBox = gui.HBox()
-        sTimeLabel = gui.Label("Start time", width="50%")
+        sTimeLabel = gui.Label("Start time:",width='50%', style={'text-align': 'right'})
         sTime = gui.Input(input_type='time', width="100%")
-        eTimeLabel = gui.Label("End time", width="50%")
-        eTime = gui.Input(input_type='time', width="100%")
-        acqDateLabel = gui.Label("Acquisition Date", width="50%")
+        eTimeLabel = gui.Label("End time: ", width="50%", style={'text-align': 'right'})
+        eTime = gui.Input(input_type='time', default_value="23:59:59", width="100%")
+        acqDateLabel = gui.Label("Acquisition Date: ", width="50%", style={'text-align': 'right'})
         acq_date_Button = gui.Date("Acq. Date", width="100%")
         #button1 = gui.Button('Input Tab', width='100%', height='100%')
 
         dateTimeHBox.append(acqDateLabel, "AcqDateLabel")
         dateTimeHBox.append(acq_date_Button, "Acquisition Date")
+        dateTimeHBox.append(spacer50Pct, "spacer0")
         dateTimeHBox.append(sTimeLabel, "Start time Label")
         dateTimeHBox.append(sTime, "Start time")
+        dateTimeHBox.append(spacer50Pct, "spacer1")
         dateTimeHBox.append(eTimeLabel, "End time Label")
         dateTimeHBox.append(eTime, "End time")
 
