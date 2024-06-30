009eee42ce6bf69c3771095fe8fe5433f768baa5
RJbalikian <46536937+RJbalikian@users.noreply.github.com>
2024-06-30 08:25:10 -0500

    got menubar aligned (filled with "INFOMATION" message for now)

--- sprit/sprit_remi.py
+++ sprit/sprit_remi.py
@@ -6,28 +6,33 @@ class SpritApp(App):
 
     def main(self):
         # DEFINE 
-        spritApp = gui.VBox(width='100%', height='100%', margin='0px auto')
+        spritApp = gui.VBox(width='100%', height='100%', margin='0px auto', style={'vertical-align':'top'})
         
-        menu = gui.Menu(width='100%', height='25px')
-        m1 = gui.MenuItem('File', width=100, height=25)
-        m2 = gui.MenuItem('View', width=100, height=25)
-        m2.onclick.do(self.menu_view_clicked)
-        m11 = gui.MenuItem('Save', width=100, height=25)
+        menu = gui.Menu(width='100%', height='25px', style={'vertical-align':'top'})
+        spritMenu = gui.MenuItem('SpRIT', width=100, height=25, style={'vertical-align':'top'})
+        settingsMenu = gui.MenuItem('Settings', width=100, height=25, style={'vertical-align':'top'})
+        
+
+        settingsMenu.onclick.do(self.menu_view_clicked)
+        m11 = gui.MenuItem('Read Data', width=100, height=25)
         m12 = gui.MenuItem('Open', width=100, height=25)
         m12.onclick.do(self.menu_open_clicked)
-        m111 = gui.MenuItem('Save', width=100, height=25)
-        m111.onclick.do(self.menu_save_clicked)
-        m112 = gui.MenuItem('Save as', width=100, height=25)
-        m112.onclick.do(self.menu_saveas_clicked)
+        m111 = gui.MenuItem('Read file', width=100, height=25)
+        m111.onclick.do(self.read_file)
+        m112 = gui.MenuItem('Read folder', width=100, height=25)
+        m112.onclick.do(self.read_folder)
 
-        menu.append([m1, m2])
-        m1.append([m11, m12])
+        menu.append([spritMenu, settingsMenu])
+        spritMenu.append([m11, m12])
         m11.append([m111, m112])
 
-        menubar = gui.MenuBar(width='100%', height='20px')
+        menubar = gui.MenuBar(width='100%', height='20px', style={'vertical-align':'top'})
         menubar.append(menu)
         
-        spritApp.append(menu)
+        infoLabel = gui.Label('INFORMATION', height='100%', margin='1%')
+
+        spritApp.append(menubar)
+        spritApp.append(infoLabel)
 
         # CLOSING SCRIPS
         tag = gui.Tag(_type='script')
@@ -46,11 +51,11 @@ class SpritApp(App):
     def menu_view_clicked(self, widget):
         pass#self.lbl.set_text('Menu clicked: View')
     
-    def menu_save_clicked(self, widget):
-        self.lbl.set_text('Menu clicked: Save')
+    def read_file(self, widget):
+        pass#self.lbl.set_text('Menu clicked: Save')
 
-    def menu_saveas_clicked(self, widget):
-        self.lbl.set_text('Menu clicked: Save As')
+    def read_folder(self, widget):
+        pass#self.lbl.set_text('Menu clicked: Save As')
 
 
     def on_window_close(self):
