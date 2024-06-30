8711d9b65af4eb14a6954090f0a3eb6b019ac5cc
RJbalikian <46536937+RJbalikian@users.noreply.github.com>
2024-06-30 07:37:42 -0500

    filemenu started working

--- sprit/sprit_remi.py
+++ sprit/sprit_remi.py
@@ -1,22 +1,56 @@
 from remi import start, App
 import remi.gui as gui
-class MyApp(App):
+class SpritApp(App):
     def __init__(self, *args):
-        super(MyApp, self).__init__(*args)
+        super(SpritApp, self).__init__(*args)
 
     def main(self):
-        wid = gui.VBox(width=320, height=320, margin='0px auto')
+        # DEFINE 
+        spritApp = gui.VBox(width='100%', height='100%', margin='0px auto')
         
+        menu = gui.Menu(width='100%', height='25px')
+        m1 = gui.MenuItem('File', width=100, height=25)
+        m2 = gui.MenuItem('View', width=100, height=25)
+        m2.onclick.do(self.menu_view_clicked)
+        m11 = gui.MenuItem('Save', width=100, height=25)
+        m12 = gui.MenuItem('Open', width=100, height=25)
+        m12.onclick.do(self.menu_open_clicked)
+        m111 = gui.MenuItem('Save', width=100, height=25)
+        m111.onclick.do(self.menu_save_clicked)
+        m112 = gui.MenuItem('Save as', width=100, height=25)
+        m112.onclick.do(self.menu_saveas_clicked)
 
-        # add the following 3 lines to your app and the on_window_close method to make the console close automatically
+        menu.append([m1, m2])
+        m1.append([m11, m12])
+        m11.append([m111, m112])
+
+        menubar = gui.MenuBar(width='100%', height='20px')
+        menubar.append(menu)
+        
+        spritApp.append(menu)
+
+        # CLOSING SCRIPS
         tag = gui.Tag(_type='script')
         tag.add_child("javascript", """window.onunload=function(e){remi.sendCallback('%s','%s');return "close?";};""" % (
             str(id(self)), "on_window_close"))
-        wid.add_child("onunloadevent", tag)
+        spritApp.add_child("onunloadevent", tag)
 
         # returning the root widget
 
-        return wid
+        return spritApp
+
+    # LISTENER FUNCTIONS
+    def menu_open_clicked(self, widget):
+        pass#self.lbl.set_text('Menu clicked: Open')
+
+    def menu_view_clicked(self, widget):
+        pass#self.lbl.set_text('Menu clicked: View')
+    
+    def menu_save_clicked(self, widget):
+        self.lbl.set_text('Menu clicked: Save')
+
+    def menu_saveas_clicked(self, widget):
+        self.lbl.set_text('Menu clicked: Save As')
 
 
     def on_window_close(self):
@@ -25,4 +59,4 @@ class MyApp(App):
         self.close()
 
 if __name__ == "__main__":
-    start(MyApp, debug=True, address='0.0.0.0', port=0)
+    start(SpritApp, debug=True, address='0.0.0.0', port=0)
