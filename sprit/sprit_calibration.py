b2d9675d62a76573d0da57b7d848beccb2bd23cb
jhurani2 <jhurani2@illinois.edu>
2024-07-09 15:20:18 -0500

    Changes to calculate_depth

--- sprit/sprit_calibration.py
+++ sprit/sprit_calibration.py
@@ -15,6 +15,7 @@ import matplotlib.pyplot as plt
 import csv
 import json
 import os
+import re
 import pathlib
 import pkg_resources
 import warnings
@@ -72,14 +73,16 @@ model_parameters = {"ISGS" : (2,1), "IbsvonA" : (96, 1.388), "IbsvonB" : (146, 1
                     "Tun" : (136, 1.357), "ThabetA": (117.13, 1.197), "ThabetB":(105.14, 0.899), "ThabetC":(132.67, 1.084), "ThabetD":(116.62, 1.169)}
 
 def calculate_depth(freq_input = {sprit_hvsr.HVSRData, sprit_hvsr.HVSRBatch, float, os.PathLike},  
-                    model = "ISGS", 
+                    model = "ISGS",
+                    site = "HVSRSite", 
                     unit = "m",
                     freq_col = "PeakFrequency", 
                     calculate_elevation = False, 
                     elevation_col = "Elevation", 
                     depth_col = "BedrockDepth", 
                     verbose = False,    #if verbose is True, display warnings otherwise not
-                    update_negative_values = False, 
+                    update_negative_values = False,
+                    export_path = None,
                     **kwargs):
     
     a = 0
@@ -100,34 +103,30 @@ def calculate_depth(freq_input = {sprit_hvsr.HVSRData, sprit_hvsr.HVSRBatch, flo
         
         for k,v in model_parameters.items():
 
-            if model.casefold() == k.casefold():
-                print("I'm here 1")  
+            if model.casefold() == k.casefold():   
                 (a, b) = model_parameters[k]
                 break
-                
-            
-            else:
-                raise ValueError("Model not found")
 
     
     elif isinstance(model, str):   #parameters a and b could be passed in as a parsable string
-        params = model.split(",")
-        list(map(float, params))
+        params = [int(s) for s in re.findall(r"[-+]?(?:\d*\.*\d+)", model)]  #figure this out later for floating points; works for integers
         (a,b) = params
-        if b >= a:                     #b should always be less than a
+        if a == 0 or b == 0:         
+            raise ValueError("Parameters cannot be zero, check model inputs")
+        elif b >= a:                     #b should always be less than a
             if verbose:
                 warn("Second parameter greater than the first, inverting values")
             (b,a) = params
-        elif a == 0 or b == 0:         
-            raise ValueError("Parameters cannot be zero, check model inputs")
+        
     
     else:
-        raise ValueError("Model not found: check inputs")
+        if (a,b) == (0, 0):
+
+            raise ValueError( "Model not found: check inputs")
 
     #Checking freq_input is a filepath
 
-    if isinstance(freq_input, os.PathLike):             #This is not working
-        print("I'm here 2")
+    if os.path.exists(freq_input):
         data = pd.read_csv(freq_input,
                             skipinitialspace= True,
                             index_col=False,
@@ -139,31 +138,48 @@ def calculate_depth(freq_input = {sprit_hvsr.HVSRData, sprit_hvsr.HVSRBatch, flo
 
         calib_data = calib_data.T
 
-    
             
         for each in range(calib_data.shape[0]):
 
             calib_data[each, 1] = a*(calib_data[each, 0]**b)
-            print("I'm here 3")
+            
 
         data["Depth to Bedrock (m)"] = calib_data[:, 1]
+        
+        if unit.casefold() in {"ft", "feet"}:
+            data["Depth to Bedrock (ft)"] = calib_data[:, 1]*3.281
+
+        if export_path is not None and os.path.exists(export_path):
+            if export_path == freq_input:
+                data.to_csv(freq_input)
+                if verbose:
+                    print("Saving data in the original file")
+
+            else:
+                if "/" in export_path:
+                    temp = os.path.join(export_path+ "/"+ site + ".csv")
+                    data.to_csv(temp)
+                
+                else:
+                    temp = os.path.join(export_path+"\\"+ site + ".csv")
+                    data.to_csv(temp)
+
+                if verbose:
+                    print("Saving data to the path specified")
+            
 
         return data
-            #     #give csv/df output with bedrock depth added
+           
 
 
 
 
-    elif model.casefold() == "all":
+    if model.casefold() == "all":
         #Statistical analysis
-        print("I'm here 4")
         sorry = True
-        
 
-    #Reading in path object
 
-    else:
-        print("I'm here because nothing worked")
+    print("I'm here because nothing worked")
 
 
 
