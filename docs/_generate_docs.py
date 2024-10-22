1b2437868825c787dc7bf4764c61cf7681000a42
RJbalikian <46536937+RJbalikian@users.noreply.github.com>
2024-10-22 14:45:15 -0500

    updates to improve linting, etc.

--- docs/_generate_docs.py
+++ docs/_generate_docs.py
@@ -10,7 +10,7 @@ import sys
 import markdown
 
 
-RELEASE_VERSION = "1.2-dev"
+RELEASE_VERSION = "1.3"
 
 VERBOSE = True
 
@@ -27,7 +27,7 @@ LINT_IT = False
 if VERBOSE:
     print('Specifying filepaths')
 
-#Setup relevant paths
+# Setup relevant paths
 currentDir = pathlib.Path((__file__)).parent
 docsDir = currentDir
 repoDir = docsDir.parent
@@ -59,7 +59,9 @@ os.environ['PYTHONPATH'] = '..' + os.pathsep + os.environ.get('PYTHONPATH', '')
 
 if VERBOSE:
     print('Updating version/release numbers')
-confFilePaths = [setupFPath, pyprojectFPath, condaFPath, confFPath, initFPath, requirePath]
+confFilePaths = [setupFPath, pyprojectFPath, condaFPath,
+                 confFPath, initFPath, requirePath]
+
 for cFile in confFilePaths:
     if cFile.exists():
         with open(cFile.as_posix(), mode='r', encoding='utf-8') as f:
@@ -94,10 +96,10 @@ for cFile in confFilePaths:
         # intended for requirements.txt
         VERTEXT = r"sprit==.*?[^\n]*"
         if 'dev' in RELEASE_VERSION:
-            relVer = RELEASE_VERSION.replace('-', '.')+'0'
+            REL_VER = RELEASE_VERSION.replace('-', '.')+'0'
         else:
-            relVer = RELEASE_VERSION        
-        NEWVERTEXT = r'sprit=='+relVer
+            REL_VER = RELEASE_VERSION
+        NEWVERTEXT = r'sprit==' + REL_VER
         cFileText = re.sub(VERTEXT, NEWVERTEXT, cFileText, flags=re.DOTALL)
 
         with open(cFile.as_posix(), mode='w', encoding='utf-8') as f:
@@ -113,7 +115,7 @@ if RTD_DOCS:
 
     if VERBOSE:
         print('Saving conf.py and index.rst so they do not get overwritten')
-    # It seems apidoc rewrites conf.py and index.rst file (don't want that), 
+    # It seems apidoc rewrites conf.py and index.rst file (don't want that),
     # so save it first and rewrite after
     confFilePath = docsDir.joinpath('conf.py')
     indFilePath = docsDir.joinpath('index.rst')
@@ -126,11 +128,13 @@ if RTD_DOCS:
     if VERBOSE:
         print('Running sphinx-apidoc')
     # Run apidoc to update api documentation from docstrings
-    subprocess.run(['sphinx-apidoc', '-F', '-M', '-e', '-f', '-o', docsDir.as_posix(), spritDir.as_posix()])
+    subprocess.run(['sphinx-apidoc', '-F', '-M', '-e', '-f', '-o',
+                    docsDir.as_posix(), spritDir.as_posix()],
+                   check=False)
 
     if VERBOSE:
         print('sphinx-apidoc complete')
-        print('Putting original conf.py and index.rst files back where I got them')
+        print('Putting original conf.py and index.rst files back')
     with open(confFilePath.as_posix(), mode='w', encoding='utf-8') as f:
         f.write(confFileText)
     with open(indFilePath.as_posix(), mode='w', encoding='utf-8') as f:
@@ -138,11 +142,11 @@ if RTD_DOCS:
 
     if VERBOSE:
         print('Cleaning make file and sources')
-    subprocess.run([docsDir.joinpath('make.bat').as_posix(), 'clean'], 
+    subprocess.run([docsDir.joinpath('make.bat').as_posix(), 'clean'],
                    check=False)
     if VERBOSE:
         print('Creating make file (for html)')
-    subprocess.run([docsDir.joinpath('make.bat').as_posix(), 'html'], 
+    subprocess.run([docsDir.joinpath('make.bat').as_posix(), 'html'],
                    check=False)
 
     if VERBOSE:
@@ -162,9 +166,11 @@ if RTD_DOCS:
                     if f2.stem in copyList:
                         shutil.copy(f2, docsDir.joinpath(f.name))
                     elif f2.stem == 'js':
-                        shutil.copy(f2.joinpath('theme.js'), docsDir.joinpath('theme.js'))
+                        shutil.copy(f2.joinpath('theme.js'),
+                                    docsDir.joinpath('theme.js'))
                     elif f2.stem == 'css':
-                        shutil.copy(f2.joinpath('theme.css'), docsDir.joinpath('theme.css'))
+                        shutil.copy(f2.joinpath('theme.css'),
+                                    docsDir.joinpath('theme.css'))
 
     for file in docsDir.iterdir():
         if file.suffix == '.html':
@@ -174,7 +180,7 @@ if RTD_DOCS:
             htmlFileText = htmlFileText.replace('src="js/', 'src="')
 
             htmlFileText = htmlFileText.replace('href="_static/', 'href="')
-            htmlFileText = htmlFileText.replace('href="css/', 'href="')            
+            htmlFileText = htmlFileText.replace('href="css/', 'href="')
             with open(file.as_posix(), mode='w', encoding='utf-8') as f:
                 f.write(htmlFileText)
 
@@ -182,34 +188,38 @@ if GITHUB_PAGES:
     # Run the pdoc command
     if RTD_THEME:
         THEME_PATH = venvPath.as_posix()+'/lib/site-packages/sphinx_RTD_THEME/'
-        subprocess.run(['pdoc', '--html', '-o', str(docsDir), '--force', '--template-dir', THEME_PATH, str(spritDir)], check=False)
+        subprocess.run(['pdoc', '--html', '-o', str(docsDir),
+                        '--force', '--template-dir', THEME_PATH, str(spritDir)], check=False)
     else:
-        subprocess.run(['pdoc', '--html', '-o', str(docsDir), '--force', str(spritDir)],check=False)
-        
-    #Set up a working directory for the files generated from pdoc
+        subprocess.run(['pdoc', '--html', '-o', str(docsDir),
+                        '--force', str(spritDir)], check=False)
+
+    # Set up a working directory for the files generated from pdoc
     workDir = pathlib.Path(os.getcwd())
-    print('workdir:',workDir)
+    print('workdir:', workDir)
     if workDir.stem == 'docs':
         pass
     elif 'docs' in str(workDir):
         pass
     else:
-        for p in pathlib.Path('.').rglob('*'): 
+        for p in pathlib.Path('.').rglob('*'):
             if 'docs\\' in str(p):
-                docsPath = pathlib.Path(str(p)[:str(p).find('docs')+ 4])
+                docsPath = pathlib.Path(str(p)[:str(p).find('docs') + 4])
                 os.chdir(docsPath)
                 break
 
-    src_path = spritDir # pathlib.Path(SUB_DIR)
-    trg_path = docsDir # src_path.parent.joinpath(output_dir) # this is main repo folder, usually
+    src_path = spritDir  # pathlib.Path(SUB_DIR)
+    trg_path = docsDir   # this is main repo folder, usually
 
     print('Reading .py files from', src_path.absolute())
     print('Placing html files in', trg_path.absolute())
 
     # Move items back into the main docs folder
-    keepList = ['generate_docs.py', 'conf.py', 'requirements.txt', 'wiki', 'pyinstaller']
+    keepList = ['generate_docs.py', 'conf.py', 'requirements.txt',
+                'wiki', 'pyinstaller']
+    
     for t in trg_path.iterdir():
-        #print('main folder', t)
+
         if t.name in keepList:
             # Don't do anything to requirements.txt or wiki folder
             pass
@@ -218,7 +228,7 @@ if GITHUB_PAGES:
                 if file.is_dir():
                     if file.name == 'resources':
                         for f in file.iterdir():
-                            # We don't want the resources folder in docs folder, remove all items
+                            # Don't want resources dir in docs dir, remove
                             os.remove(f)
                     elif file.name == 'wiki':
                         # Keep wiki folder
@@ -229,7 +239,7 @@ if GITHUB_PAGES:
                             if destFilePath.exists():
                                 os.remove(destFilePath)
                             f = f.rename(destFilePath)
-                            keepList.append(destFilePath.name) #Already deleted and replaced, want to keep it now
+                            keepList.append(destFilePath.name)  # Already deleted and replaced, want to keep it now
                     if file.name not in keepList and file.parent.name != 'wiki':
                         os.rmdir(file)
                 else:
@@ -238,11 +248,11 @@ if GITHUB_PAGES:
                         os.remove(destFilePath)
                     file = file.rename(destFilePath)
                     keepList.append(destFilePath.name)
-                    if file.name=='index.html':
+                    if file.name == 'index.html':
                         mainhtmlFPath = file.parent.joinpath('main.html')
                         if mainhtmlFPath.is_file():
                             os.remove(mainhtmlFPath)
-                        file.rename(mainhtmlFPath)      
+                        file.rename(mainhtmlFPath)
             if t.name not in keepList:
                 os.rmdir(t)
         else:
@@ -262,8 +272,8 @@ if GITHUB_PAGES:
         html = html.replace('| Dependency', '<table>\n\t<tr>\n\t<th>Dependency</th>\n', 1)
         html = html.replace('| Link', '\t\t<th>Link</th>\n', 1)
         html = html.replace('| Description', '\t\t<th>Description</th>\n', 1)
-        html = html.replace('|','</tr>', 1)
-        html = html.replace('|------------|----------------------------|-------------------------------------------------------------------------------------------------|','', 1)
+        html = html.replace('|', '</tr>', 1)
+        html = html.replace('|------------|----------------------------|-------------------------------------------------------------------------------------------------|', '', 1)
 
         html = html.replace('|', '\t<tr>\n\t\t<td>', 1)
         html = html.replace('|', '\t</td>\n\t\t<td>', 2)
@@ -291,10 +301,10 @@ if GITHUB_PAGES:
             f.write(html)
         print('hmtl landing page:', dst)
     else:
-        #Copy main readme file into docs so github pages will read it
+        # Copy main readme file into docs so github pages will read it
         shutil.copy(src=str(readmePath.as_posix()), dst='.')
 
-    #Update setup file(s) with release version number
+    # Update setup file(s) with release version number
     setupFPath = repoDir.joinpath('setup.py')
     pyprojectFPath = repoDir.joinpath('pyproject.toml')
     condaFPath = repoDir.joinpath('conda/meta.yaml')
@@ -304,7 +314,7 @@ if GITHUB_PAGES:
         with open(cFile.as_posix(), mode='r', encoding='utf-8') as f:
             cFileText = f.read()
 
-        #Update which file is being analyzed for creating exe
+        # Update which file is being analyzed for creating exe
         VER_TEXT = r'version=".*?"'
         NEW_VER_TEXT = r'version="'+RELEASE_VERSION+'"'
         cFileText = re.sub(VER_TEXT, NEW_VER_TEXT, cFileText, flags=re.DOTALL)
@@ -313,11 +323,9 @@ if GITHUB_PAGES:
         NEW_VER_TEXT = r'version: '+RELEASE_VERSION
         cFileText = re.sub(VER_TEXT, NEW_VER_TEXT, cFileText, flags=re.DOTALL)
 
-
         VER_TEXT = r'git_tag:\s+v+\d+\.\d+\.\d+[^\n]*'
         NEW_VER_TEXT = r'git_tag: v'+RELEASE_VERSION
         cFileText = re.sub(VER_TEXT, NEW_VER_TEXT, cFileText, flags=re.DOTALL)
-
 
         with open(cFile.as_posix(), mode='w', encoding='utf-8') as f:
             f.write(cFileText)
@@ -329,14 +337,14 @@ if LINT_IT:
         print(fileP)
         print(f'\nLINTING {fileP.as_posix()}')
         ignoreList = ['E501']
-        strIgnoreList =  "--ignore="+str(str(ignoreList)[1:-1].replace(' ', '').replace("'",""))
-        result = subprocess.run(['flake8', strIgnoreList, fileP.as_posix(),], 
+        STR_IGNORE_LIST = "--ignore=" + str(str(ignoreList)[1:-1].replace(' ', '').replace("'", ""))
+        result = subprocess.run(['flake8', STR_IGNORE_LIST, fileP.as_posix(),],
                                 stdout=subprocess.PIPE, check=False)
         print(result.stdout.decode('utf-8'))
 
 if RUN_TESTS:
     print('Testing sprit.run()')
-    SHELL_TYPE=True
+    SHELL_TYPE = True
     if sys.platform == 'linux':
         SHELL_TYPE = False
     try:
