f08d57fcae6b37fa12acb65c4018ec12d9e7224e
Riley <rileybalikian@outlook.com>
2025-06-25 14:16:41 -0500

    2.8.6 update docs and vernum

--- docs/_generate_docs.py
+++ docs/_generate_docs.py
@@ -9,7 +9,7 @@ import sys
 
 import markdown
 
-RELEASE_VERSION = "2.8.5"
+RELEASE_VERSION = "2.8.6"
 
 VERBOSE = True
 
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name="sprit",
     author= "Riley Balikian",
     author_email = "balikian@illinois.edu",
-    version="2.8.5",
+    version="2.8.6",
     package_data={'sprit': ['resources/*', 'resources/icon/*', 'resources/themes/*', 'resources/themes/forest-dark/*', 
                             'resources/themes/forest-light/*', 'resources/sample_data/*','resources/settings/*']},
     long_description_content_type="text/markdown",
